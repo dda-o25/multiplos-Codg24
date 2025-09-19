778b94cadb57bbb60ccdf1d9426ebe5c18415b6b
Codg24 <carlos.diaz@iteso.mx>
2025-09-18 23:15:54 -0600

    Primera solución

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,26 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Integrantes
+Carlos Osvaldo Díaz García
+
+Fecha
+12 de septiembre de 2025
+
+Dados dos números, determinar si uno es múltiplo del otro.
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero_1 = int(input("Introduzca un número: "))
+numero_2 = int(input("Introduzca otro número: "))
 
 # Proceso
 
+if numero_1 % numero_2 == 0:
+    #Salida
+    print("El número", numero_1,"es múltiplo del", numero_2)
 
-# Salidas
-print(salida)
+if numero_2 % numero_1 == 0:
+    #Salida
+    print("El número", numero_2,"es múltiplo del", numero_1)
+else:
+    #Salida
+    print("Ninguno de los números es múltiplo del otro")
