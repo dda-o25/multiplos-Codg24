b560935b0e1a117ebdcb1ac079638640e536c01e
Codg24 <carlos.diaz@iteso.mx>
2025-09-18 23:30:55 -0600

    Quinta solución

--- multiplos.py
+++ multiplos.py
@@ -14,19 +14,22 @@ numero_2 = int(input("Introduzca otro número: "))
 
 # Proceso
 
+if numero_1>0 and numero_2>0:
+    if numero_1 % numero_2 == 0:
+        #Salida
+        print("El número", numero_1,"es múltiplo del", numero_2)
+
+    if numero_2 % numero_1 == 0:
+        #Salida
+        print("El número", numero_2,"es múltiplo del", numero_1)
+    else:
+        #Salida
+        print("Ninguno de los números es múltiplo del otro")
+
 if numero_1 == 0:
-    #Salida
-    print("El número", numero_1,"es múltiplo del", numero_2)
-elif numero_1 % numero_2 == 0:
     #Salida
     print("El número", numero_1,"es múltiplo del", numero_2)
 
 if numero_2 == 0:
     #Salida
     print("El número", numero_2,"es múltiplo del", numero_1)
-elif numero_2 % numero_1 == 0:
-    #Salida
-    print("El número", numero_2,"es múltiplo del", numero_1)
-else:
-    #Salida
-    print("Ninguno de los números es múltiplo del otro")
